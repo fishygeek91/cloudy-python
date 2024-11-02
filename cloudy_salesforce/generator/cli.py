1d15c5a4aef1a4fc6890e4cebcf44e4809ac15fb
fishygeek91 <fishygeek91@gmail.com>
2024-11-02 09:41:50 -0600

    - cleaning up

--- cloudy_salesforce/generator/cli.py
+++ cloudy_salesforce/generator/cli.py
@@ -12,14 +12,9 @@ def generate(args):
     """
     Handle the 'generate' command.
     """
-    print(find_dotenv(raise_error_if_not_found=True))
-    load_dotenv(dotenv_path=find_dotenv(raise_error_if_not_found=True))
-    print("Environment Variables:", os.environ)
     sobjects = args.sobjects
     alias_name = args.alias
-    print(f"Generating code with option1={sobjects} and option2={alias_name}")
-    # Implement your code generation logic here
-    # load json file .cloudy_config to get auth details and object names
+    print(f"Generating code with sobjects={sobjects} and alias={alias_name}")
 
     # load auth details
     with open(".cloudy_config", "r") as file:
@@ -35,34 +30,26 @@ def generate(args):
     alias = auth_details["aliases"][alias_name]
 
     def get_auth(alias):
-
         if alias["type"] == "basic":
-            username = os.getenv("PROD_USERNAME")
+            load_dotenv(dotenv_path=find_dotenv(raise_error_if_not_found=True))
+            username = os.getenv(alias["credentials"]["username"])
             password = os.getenv(alias["credentials"]["password"])
             security_token = os.environ.get(alias["credentials"]["security_token"])
-            print(username)
-            print(alias["credentials"]["username"])
-            print(password)
-            print(security_token)
-            return "yo"
+
             # need to add params like sandbox and url
-            # return UsernamePasswordAuthentication(
-            #     username=username, password=password, security_token=security_token
-            # )
+            return UsernamePasswordAuthentication(
+                username=username, password=password, security_token=security_token
+            )
 
         raise Exception(f"Auth type not supported yet: {alias['type']}")
 
     auth = get_auth(alias)
 
-    # generator = SObjectGenerator(auth)
-    # generator.generate_all(sobjects)
+    generator = SObjectGenerator(auth)
+    generator.generate_all(sobjects)
 
 
 def main():
-    path_to_env = os.path.join(os.path.dirname(__file__), ".env")
-    print("hello")
-    print(os.path.dirname(__file__))
-    load_dotenv(path_to_env)
     parser = argparse.ArgumentParser(
         description="Your Package CLI", usage="%(prog)s [command] [options]"
     )
